2e3a4b44f4a5756006707c3337fb0a0a0f5c3160
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-16 22:09:22 +0300

    выравнил код

--- Program.cs
+++ Program.cs
@@ -14,12 +14,12 @@ int number3 = int.Parse(input3);
 
 int max = number1;
 
-if(number2 > max)
+if (number2 > max)
 {
-max = number2;
-} 
-if(number3 > max)
+    max = number2;
+}
+if (number3 > max)
 {
-max = number3;
+    max = number3;
 }
 Console.WriteLine(max);
